369d863b60cbdf5f81856c93f0fb03efd3efe27c
q02031990 <quy02031990@gmail.com>
2015-11-07 10:25:52 +0100

    hoan thien bai tap

--- Ubung2/src/QStreamTokenizer.java
+++ Ubung2/src/QStreamTokenizer.java
@@ -13,18 +13,33 @@ public class QStreamTokenizer {
 		StreamTokenizer st = new StreamTokenizer(qReader);
 		
 		boolean _ende = false;
+		double Tong =0.0;
+		int i=1;
 		do{
 			int token = st.nextToken();
 			switch(token){
 			case StreamTokenizer.TT_EOF:
-				System.out.println("End of file");
+				if(i>1){
+           		 System.out.println("Durchschnitt: " + Tong/i);
+           		 System.out.println("------------------------------------------");
+           	 	}
+                Tong = 0;
+                i=0;
 				_ende =true;
 				break;
              case StreamTokenizer.TT_WORD:
+            	 if(i>1){
+            		 System.out.println("Durchschnitt: " + Tong/i);
+            		 System.out.println("------------------------------------------");
+            	 }
                 System.out.println("Word: " + st.sval);
+                Tong = 0;
+                i=0;
                 break;
              case StreamTokenizer.TT_NUMBER:
                 System.out.println("Number: " + st.nval);
+                i++;
+                Tong = Tong + st.nval;
                 break;
              default:
             	 System.out.println((char)token + " encountered");
